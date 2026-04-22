f0abe677a4a4e0f9b60435ef7519c13aeab0bacc
RocketSmash <118481037+RocketSmash9000@users.noreply.github.com>
2026-04-22 17:20:04 +0200

    Fixed tlp status command
    Fixed the tlp stat command so that it works as it should

--- autopowersaver.sh
+++ autopowersaver.sh
@@ -3,7 +3,7 @@
 # returns battery percentage without % symbol
 percentage=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | sed 's/[^0-9]//g')
 status=$(upower -i $(upower -e | grep 'BAT') | grep -m 1 -o "discharging")
-tlp_running=$(tlp-status | grep -m 1 -o 'battery')
+tlp_running=$(tlp-stat -s | grep -m 1 -o 'battery')
 
 if [[ percentage -lt 95 && status == "discharging" && tlp_running != 'battery' ]] then
 	tlp power-saver
